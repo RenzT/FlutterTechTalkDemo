f28151b06a97290a81001bed7f7c58ed9fc65ee7
Tarik Renz <t_renz@outlook.de>
2021-06-09 18:03:36 +0200

    Added Add Item Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<String> names = <String>[];
 
-  final List<String> names = <String>['Aby', 'Aish', 'Ayan', 'Ben', 'Bob', 'Charlie', 'Cook', 'Carline'];
+  TextEditingController nameController = TextEditingController();
+
+  void addItemToList() {
+    setState(() {
+      names.insert(0, nameController.text);
+    });
+  }
+
+  void clearText() {
+    nameController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,27 +49,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: names.length,
-        itemBuilder: (BuildContext context, int index){
-          return Container(
-            height: 50,
-            margin: EdgeInsets.all(2),
-            child: Center(
-              child: Text('${names[index]}',
+          padding: const EdgeInsets.all(8),
+          itemCount: names.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              margin: EdgeInsets.all(2),
+              child: Center(
+                  child: Text(
+                '${names[index]}',
                 style: TextStyle(fontSize: 18),
-              )
-            ),
-          );
-        }
-        ),
+              )),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialog(context),
-            );
-          },
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _buildPopupDialog(context),
+          );
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
@@ -66,23 +76,30 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildPopupDialog(BuildContext context) {
-  return new AlertDialog(
-    title: const Text('Popup example'),
-    content: new Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-      new TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: const Text('Close'),
+    return new AlertDialog(
+      title: const Text('Add an Item'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Add Item to ToDo List'),
+          ),
+        ],
       ),
-    ],
-  );
-}
+      actions: <Widget>[
+        new TextButton(
+          onPressed: () {
+            addItemToList();
+            clearText();
+            Navigator.of(context).pop();
+          },
+          child: const Text('Add'),
+        ),
+      ],
+    );
+  }
 }
